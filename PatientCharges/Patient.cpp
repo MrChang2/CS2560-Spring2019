1f1e7daff44cbbe7d1398d24f00aa420258755fc
MrChang2 <43765332+MrChang2@users.noreply.github.com>
2019-04-10 21:41:38 -0700

    Update and rename Patient.cpp.txt to Patient.cpp

---
+++ PatientCharges/Patient.cpp
@@ -0,0 +1,123 @@
+Patient::~Patient()
+{
+}
+/**
+*@brief Assigns parameter string n to name
+*/
+void Patient::setName(string n) {
+	name = n;
+}
+/**
+*@brief Returns the name private variable
+*/
+string Patient::getName() {
+	return name;
+}
+/**
+*@brief Assigns the parameter string n to middleName
+*/
+void Patient::setMiddleName(string n) {
+	middleName = n;
+}
+/**
+*@brief Returns the middleName private variable
+*/
+string Patient::getMiddleName() {
+	return middleName;
+}
+/**
+*@brief Assigns the parameter string n to lastName
+*/
+void Patient::setLastName(string n) {
+	lastName = n;
+}
+/**
+*@brief Returns the lastName private variable
+*/
+string Patient::getLastName() {
+	return lastName;
+}
+/**
+*@brief Assigns the parameter string n to address
+*/
+void Patient::setAddress(string n) {
+	address = n;
+}
+/** 
+*@brief Returns the address private variable
+*/
+string Patient::getAddress() {
+	return address;
+}
+/**
+*@brief Assigns the parameter string n to city
+*/
+void Patient::setCity(string n) {
+	city = n;
+}
+/**
+*@brief Returns the city private variable
+*/
+string Patient::getCity() {
+	return city;
+}
+/**
+*@brief Assigns the parameter string n to state
+*/
+void Patient::setState(string n) {
+	state = n;
+}
+/**
+*@brief Returns the state private variable
+*/
+string Patient::getState() {
+	return state;
+}
+/**
+*@brief Assigns the parameter string n to zip
+*/
+void Patient::setZip(string n) {
+	zip = n;
+}
+/**
+*@brief Returns the zip private variable
+*/
+string Patient::getZip() {
+	return zip;
+}
+/**
+*@brief Assigns the parameter string n to phoneNumber
+*/
+void Patient::setPhoneNumber(string n) {
+	phoneNumber = n;
+}
+/**
+*@brief Returns the phoneNumber private variable
+*/
+string Patient::getPhoneNumber() {
+	return phoneNumber;
+}
+/**
+*@brief Assigns the parameter string n to emergencyName
+*/
+void Patient::setEmergencyName(string n) {
+	emergencyName = n;
+}
+/**
+*@brief Returns the emergencyName private variable
+*/
+string Patient::getEmergencyName() {
+	return emergencyName;
+}
+/**
+*@brief Assigns the parameter string n to emergencyNumber
+*/
+void Patient::setEmergencyNumber(string n) {
+	emergencyNumber = n;
+}
+/**
+*@brief Returns the emergencyNumber private variable
+*/
+string Patient::getEmergencyNumber() {
+	return emergencyNumber;
+}
