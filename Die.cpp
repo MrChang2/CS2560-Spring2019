4f9dd2dffdb48940af7f40b916b16b5d7815cf53
MrChang2 <43765332+MrChang2@users.noreply.github.com>
2019-04-10 21:47:13 -0700

    Update Die.cpp

--- Die.cpp
+++ Die.cpp
@@ -1,7 +1,14 @@
+/**
+* @file
+* @brief Program to provide functionality to the dice class.
+*/
 #include "Die.h"
 #include <cstdlib>
 #include <ctime>
 
+/**
+* @brief Constructor takes int and assigns it to sides variable. Also assigns first value through roll()
+*/
 Die::Die(int numSides)
 {
 	unsigned seed = time(0);
@@ -12,13 +19,23 @@ Die::Die(int numSides)
 
 	roll();
 }
+/**
+* Method generates a random number through rand() and assigns it to value
+*/
 void Die::roll() {
 	const int MIN_VALUE = 1;	//minimum die value
 	value = (rand() % (sides - MIN_VALUE + 1)) + MIN_VALUE;
 }
+/**
+* @brief Returns number of sides. Should always be 6
+*/
 int Die::getSides() {
 	return sides;
 }
+/**
+* Returns current value of dice. Always possible because roll() is called in constructor
+*/
 int Die::getValue() {
 	return value;
 }
+
