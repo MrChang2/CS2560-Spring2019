87bd8c5e7d6e907f00e269510cba41d33b6bafa1
MrChang2 <43765332+MrChang2@users.noreply.github.com>
2019-04-10 21:46:48 -0700

    Update Die.h

--- Die.h
+++ Die.h
@@ -1,3 +1,7 @@
+/**
+* @file
+@brief Header file to serve as the structure for the dice class.
+*/
 #pragma once
 #ifndef DIE_H
 #define DIE_H
@@ -5,13 +9,13 @@
 class Die
 {
 private:
-	int sides;	//number of sides
-	int value;	//the die's value
+	int sides;	/**Records the number of sides for the dice*/
+	int value;	/**Holds and records the dice's value*/
 public:
-	Die(int = 6);	//Constructor
-	void roll();	//rolls the die
-	int getSides();	//returns the number of sides
-	int getValue();	//returns the die's value
+	Die(int = 6);	/**Basically the default constructor, setting the number of sides to 6*/
+	void roll();	/**Method to roll the dice by randomly generating a value between 1 and 6. Number is assigned to value*/
+	int getSides();	/**Method to return number of sides, should always be 6*/
+	int getValue();	/**Method to return current value of dice*/
 };
 #endif
 
